34b86b6b8b607e17f09d01319275a00a59f102ee
Robnibourg93 <robnibourg@hotmail.nl>
2025-03-02 00:02:26 +0100

    updated landing page

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,14 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+window.addEventListener('load',function(){
+    var title = document.querySelector('.greet'),
+        subtext = document.querySelector('.subTexts'),
+        delay = 1000;
 
+
+    setTimeout(function(){title.style.top='0';},delay);
+    setTimeout(function(){subtext.style.bottom = '0%';},delay*2);
+
+});
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
