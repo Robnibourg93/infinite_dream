7373c03295a823d20774349b1299c10d0d446e37
Robnibourg93 <robnibourg@hotmail.nl>
2025-03-03 15:17:11 +0100

    refactor to maintain consistency
    added button to landing page

--- assets/app.js
+++ assets/app.js
@@ -6,14 +6,40 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-window.addEventListener('load',function(){
+
+window.addEventListener('load', function () {
     var title = document.querySelector('.greet'),
-        subtext = document.querySelector('.subTexts'),
+        subtext = document.querySelector('.sub-texts'),
+        footer = document.querySelector('.footer'),
+        button = document.createElement("button"),
         delay = 1000;
 
+    button.className += "enter-button";
+    button.innerHTML = 'ENTER'
 
-    setTimeout(function(){title.style.top='0';},delay);
-    setTimeout(function(){subtext.style.bottom = '0%';},delay*2);
+    setTimeout(function () {
+        title.style.top = '0';
+    }, delay);
+    setTimeout(function () {
+        subtext.style.bottom = '0%';
+    }, delay * 2);
+    setTimeout(function () {
+        footer.style.opacity = '1'
+    }, delay * 3);
+    setTimeout(function () {
+        footer.style.opacity = '0'
+    }, delay * 5);
+    setTimeout(function () {
+        footer.removeChild(document.querySelector('.warcraftLogo'))
+    }, delay * 6);
+    setTimeout(function () {
+        footer.appendChild(button)
+    }, delay * 6);
+    setTimeout(function () {
+        footer.style.opacity = '1'
+    }, delay * 7);
 
 });
+
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
